c4f6fb2679df08e0079ceb0d565f60fd5f7bc997
David Delassus <david.jose.delassus@gmail.com>
2020-05-21 11:26:03 +0000

    clean: drastic clean of obsolete structure

--- sources/project-github-webhook/src/server.js
+++
@@ -1,6 +0,0 @@
-import http from 'http'
-import handlers from './handlers'
-
-import { port } from './config'
-
-http.createServer(handlers).listen(port || 8000)
--- sources/release-controller/tests/handler.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert')
-
-describe('handler', () => {
-    it('should return a PipelineRun when a Release is created', () => {
-        assert.equal(true, true)
-    })
-})
