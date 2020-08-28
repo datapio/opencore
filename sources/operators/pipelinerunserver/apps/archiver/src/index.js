bcd335b41c9d974e6c0fbe8f9193c0cb57eb2e98
David Delassus <david.jose.delassus@gmail.com>
2020-08-28 15:40:20 +0200

    fix(operators/pipelinerunserver): remove babel and rollup

--- sources/operators/pipelinerunserver/apps/archiver/src/index.js
+++ sources/operators/pipelinerunserver/apps/archiver/src/index.js
@@ -1,50 +1,56 @@
-import { operator, adopt_resource } from '@datapio/k8s-operator'
-import { make_engine } from '@datapio/amqp-engine'
+const { make_operator, adopt_resource } = require('@datapio/sdk-k8s-operator')
+const { make_engine } = require('@datapio/sdk-amqp-engine')
 
-operator({
-  lifecycle: {
-    initialize: async (kubectl) => ({
-      rabbitmq: await make_engine({
-        url: process.env.RABBITMQ_URL,
-        consumers: {
-          history: {
-            queue: process.env.RABBITMQ_HISTORY_QUEUE,
-            handler: async (publishers, pipeline_run_request) => {
-              const runrequests = await kubectl.list({
-                apiVersion: 'datap.io/v1alpha1',
-                kind: 'PipelineRunRequest',
-                name,
-                namespace,
-                fieldSelector: {
-                  'status.archived': true
-                }
-              })
+const main = async () => {
+  const operator = await make_operator({
+    lifecycle: {
+      initialize: async (kubectl) => ({
+        rabbitmq: await make_engine({
+          url: process.env.RABBITMQ_URL,
+          consumers: {
+            history: {
+              queue: process.env.RABBITMQ_HISTORY_QUEUE,
+              handler: async (publishers, pipeline_run_request) => {
+                const runrequests = await kubectl.list({
+                  apiVersion: 'datap.io/v1alpha1',
+                  kind: 'PipelineRunRequest',
+                  name,
+                  namespace,
+                  fieldSelector: {
+                    'status.archived': true
+                  }
+                })
 
-              await Promise.all(runrequests
-                .sort(by_completion_time)
-                .slice(0, 0 - parseInt(process.env.ARCHIVER_HISTORY_SIZE || '10'))
-                .map(async ({ apiVersion, kind, metadata: { name, namespace }}) =>
-                  await kubectl.delete({ apiVersion, kind, name, namespace })
+                await Promise.all(runrequests
+                  .sort(by_completion_time)
+                  .slice(0, 0 - parseInt(process.env.ARCHIVER_HISTORY_SIZE || '10'))
+                  .map(async ({ apiVersion, kind, metadata: { name, namespace }}) =>
+                    await kubectl.delete({ apiVersion, kind, name, namespace })
+                  )
                 )
-              )
 
-              const { name, namespace } = pipeline_run_request.metadata
-              await kubectl.patch({
-                apiVersion: 'datap.io/v1alpha1',
-                kind: 'PipelineRunRequest',
-                name,
-                namespace,
-                status: {
-                  archived: true
-                }
-              })
+                const { name, namespace } = pipeline_run_request.metadata
+                await kubectl.patch({
+                  apiVersion: 'datap.io/v1alpha1',
+                  kind: 'PipelineRunRequest',
+                  name,
+                  namespace,
+                  status: {
+                    archived: true
+                  }
+                })
+              }
             }
           }
-        }
-      })
-    }),
-    terminate: async (kubectl, { rabbitmq }) => {
-      await rabbitmq.cancel()
+        })
+      }),
+      terminate: async (kubectl, { rabbitmq }) => {
+        await rabbitmq.cancel()
+      }
     }
-  }
-})
+  })
+
+  await operator.join()
+}
+
+main()
--- sources/operators/pipelinerunserver/apps/worker/src/index.js
+++ sources/operators/pipelinerunserver/apps/worker/src/index.js
@@ -1,84 +1,90 @@
-import { operator, adopt_resource } from '@datapio/k8s-operator'
-import { make_engine } from '@datapio/amqp-engine'
+const { make_operator, adopt_resource } = require('@datapio/sdk-k8s-operator')
+const { make_engine } = require('@datapio/sdk-amqp-engine')
 
-operator({
-  lifecycle: {
-    initialize: async (kubectl) => ({
-      rabbitmq: await make_engine({
-        url: process.env.RABBITMQ_URL,
-        publishers: {
-          history: {
-            queue: process.env.RABBITMQ_HISTORY_QUEUE
-          }
-        },
-        consumers: {
-          worker: {
-            queue: process.env.RABBITMQ_WORKER_QUEUE,
-            handler: async ({ history }, pipeline_run_request) => {
-              const { metadata: { name, namespace } } = pipeline_run_request.metadata
-              const {
-                pipeline,
-                server,
-                extraResources,
-                resources,
-                workspaces,
-                params
-              } = pipeline_run_request.spec
-
-              await kubectl.create(...extraResources.map(
-                resource => adopt_resource(pipeline_run_request, resource)
-              ))
-
-              const prun = await kubectl.create(adopt_resource({
-                apiVersion: 'tekton.dev/v1alpha1',
-                kind: 'PipelineRun',
-                metadata: {
-                  name,
-                  namespace
-                },
-                spec: {
-                  pipelineRef: {
-                    name: pipeline
-                  },
+const main = async () => {
+  const operator = await make_operator({
+    lifecycle: {
+      initialize: async (kubectl) => ({
+        rabbitmq: await make_engine({
+          url: process.env.RABBITMQ_URL,
+          publishers: {
+            history: {
+              queue: process.env.RABBITMQ_HISTORY_QUEUE
+            }
+          },
+          consumers: {
+            worker: {
+              queue: process.env.RABBITMQ_WORKER_QUEUE,
+              handler: async ({ history }, pipeline_run_request) => {
+                const { metadata: { name, namespace } } = pipeline_run_request.metadata
+                const {
+                  pipeline,
+                  server,
+                  extraResources,
                   resources,
                   workspaces,
                   params
-                }
-              }))
+                } = pipeline_run_request.spec
 
-              const final_prun_req = await kubectl.wait_condition({
-                apiVersion: pipeline_run_request.apiVersion,
-                kind: pipeline_run_request.kind,
-                name,
-                namespace,
-                callback: async object => {
-                  const prun_req = await kubectl.patch({
-                    apiVersion: pipeline_run_request.apiVersion,
-                    kind: pipeline_run_request.kind,
+                await kubectl.create(...extraResources.map(
+                  resource => adopt_resource(pipeline_run_request, resource)
+                ))
+
+                const prun = await kubectl.create(adopt_resource({
+                  apiVersion: 'tekton.dev/v1alpha1',
+                  kind: 'PipelineRun',
+                  metadata: {
                     name,
-                    namespace,
-                    patch: {
-                      status: {
-                        pipelineRunStatus: object.status
-                      }
-                    }
-                  })
-
-                  return {
-                    condition: object.status.completionTime,
-                    result: prun_req
+                    namespace
+                  },
+                  spec: {
+                    pipelineRef: {
+                      name: pipeline
+                    },
+                    resources,
+                    workspaces,
+                    params
                   }
-                }
-              })
+                }))
 
-              await history.send(final_prun_req)
+                const final_prun_req = await kubectl.wait_condition({
+                  apiVersion: pipeline_run_request.apiVersion,
+                  kind: pipeline_run_request.kind,
+                  name,
+                  namespace,
+                  callback: async object => {
+                    const prun_req = await kubectl.patch({
+                      apiVersion: pipeline_run_request.apiVersion,
+                      kind: pipeline_run_request.kind,
+                      name,
+                      namespace,
+                      patch: {
+                        status: {
+                          pipelineRunStatus: object.status
+                        }
+                      }
+                    })
+
+                    return {
+                      condition: object.status.completionTime,
+                      result: prun_req
+                    }
+                  }
+                })
+
+                await history.send(final_prun_req)
+              }
             }
           }
-        }
-      })
-    }),
-    terminate: async (kubectl, { rabbitmq }) => {
-      await rabbitmq.cancel()
+        })
+      }),
+      terminate: async (kubectl, { rabbitmq }) => {
+        await rabbitmq.cancel()
+      }
     }
-  }
-})
+  })
+
+  await operator.join()
+}
+
+main()
