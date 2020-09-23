63092552802a7219024e7cd3d790625acb594644
David Delassus <david.jose.delassus@gmail.com>
2020-09-23 21:07:23 +0200

    fix(ci): test condition in maybe-publish.sh

--- .github/scripts/maybe-publish.sh
+++ .github/scripts/maybe-publish.sh
@@ -5,7 +5,7 @@ local_version=$(cat package.json | jq -r .version)
 upstream_versions=$(yarn info $package --json | jq -r '.data.versions | values[] as $v | $v')
 echo $upstream_version | grep $local_version
 not_upstream=$?
-if [ $not_upstream -eq 1 ]
+if [[ $not_upstream -eq 1 ]]
 then
   yarn publish
 fi
