b3d21826c81d2f9340295356188c22a6d4222c60
David Delassus <david.jose.delassus@gmail.com>
2020-09-24 22:51:27 +0200

    fix(ci): typo and logging

--- .github/scripts/maybe-publish.sh
+++ .github/scripts/maybe-publish.sh
@@ -3,9 +3,16 @@
 package=$1
 local_version=$(cat package.json | jq -r .version)
 upstream_versions=$(yarn info $package --json | jq -r '.data.versions | values[] as $v | $v')
-echo $upstream_version | grep $local_version
+echo $upstream_versions | grep $local_version
 not_upstream=$?
-if [[ $not_upstream -eq 1 ]]
+
+echo "Upstream versions:"
+echo $upstream_versions
+
+echo "Local version:"
+echo $local_version
+
+if [ $not_upstream -eq 1 ]
 then
   yarn publish
 fi
