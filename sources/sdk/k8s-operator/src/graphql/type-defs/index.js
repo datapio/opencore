01fad59e5b82d622aa5f79b7bcd3e26654439369
David Delassus <david.jose.delassus@gmail.com>
2020-10-25 03:55:50 +0100

    :sparkles: add GraphQL utilities

---
+++ sources/sdk/k8s-operator/src/graphql/type-defs/index.js
@@ -0,0 +1,18 @@
+const gql = require('apollo-server-express')
+const path = require('path')
+const fs = require('fs')
+
+const requireTypeDef = name =>
+  gql(fs.readFileSync(
+    path.resolve(__dirname, `./${name}.graphql`),
+    { encoding: 'utf8' }
+  ))
+
+module.exports = {
+  Label: requireTypeDef('label'),
+  Annotation: requireTypeDef('annotation'),
+  NamespacedResource: requireTypeDef('namespaced-resource'),
+  ClusterResource: requireTypeDef('cluster-resource'),
+  ResourceField: requireTypeDef('resource-field'),
+  Collection: requireTypeDef('collection')
+}
