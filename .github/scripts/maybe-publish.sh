4342654658b3e2c3d289b655bc66388f56d1f496
David Delassus <david.jose.delassus@gmail.com>
2020-11-01 04:16:04 +0100

    :bug: :construction_worker: fix grep command to check if npm publish is needed

--- .github/scripts/maybe-publish.sh
+++ .github/scripts/maybe-publish.sh
@@ -3,7 +3,7 @@
 package=$1
 local_version=$(cat package.json | jq -r .version)
 upstream_versions=$(yarn info $package --json | jq -r '.data.versions | values[] as $v | $v')
-echo $upstream_versions | grep $local_version
+echo $upstream_versions | grep -E "^${local_version}$"
 not_upstream=$?
 
 echo "Upstream versions:"
