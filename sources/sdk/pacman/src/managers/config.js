eef620be8bcd9ee37df88201282d67fb6ecaab77
David Delassus <david.jose.delassus@gmail.com>
2020-12-03 04:36:17 +0100

    :sparkles: add config manager #sdk/pacman

---
+++ sources/sdk/pacman/src/managers/config.js
@@ -0,0 +1,24 @@
+// @flow
+
+import { promises as fs } from 'fs'
+
+export type Configuration = {
+  pipelines: Array<string>
+}
+
+export type Event = {
+  kind: 'push' | 'pull_request' | 'local'
+}
+
+const parseFile = async (filename: string): any =>
+  JSON.parse(await fs.readFile(filename, 'utf-8'))
+
+export default {
+  get: (configPath: string, eventPath: string): Promise<[Configuration, Event]> => {
+    return Promise.all([
+      parseFile(configPath),
+      parseFile(eventPath)
+    ])
+  }
+}
+
