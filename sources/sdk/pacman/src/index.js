108fdb01bfdd34fe3762be05016128ad24fe19e1
David Delassus <david.jose.delassus@gmail.com>
2020-12-03 04:38:09 +0100

    :sparkles: use sywac to build CLI #sdk/pacman

--- sources/sdk/pacman/src/index.js
+++ sources/sdk/pacman/src/index.js
@@ -1,19 +1,34 @@
-import { Pipeline } from './api'
+// @flow
 
-const main = async () => {
-  const argv = process.argv.slice(2)
+import Api from 'sywac/api'
+import configManager from './managers/config'
+import pipelineManager from './managers/pipeline'
 
-  return do {
-    if (argv.length !== 1) {
-      console.error('Usage: datapio-pacman [pipeline-module]')
-      1
-    }
-    else {
-      const [module] = argv
-      const pipeline: Pipeline = await import(module)
-      console.log(pipeline)
-    }
-  }
+const cli = new Api()
+  .positional('<config:file>', {
+    paramsDesc: 'Path to configuration file',
+    mustExist: true
+  })
+  .positional('<event:file>', {
+    paramsDesc: 'Path to JSON payload describing the event',
+    mustExist: true
+  })
+
+const main = async (): Promise<number> => {
+  const argv = await cli.parseAndExit()
+  const [config, event] = await configManager.get(argv.config, argv.event)
+
+  const pipelines = await pipelineManager.load(config.pipelines)
+  await Promise.all(pipelines.map(
+    pipeline => pipelineManager.run(pipeline, event)
+  ))
+
+  return 0
 }
 
-main().catch(console.error)
+main()
+  .then((exitCode: number) => process.exit(exitCode))
+  .catch((err: Error) => {
+    console.error(err)
+    process.exit(1)
+  })
