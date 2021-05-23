8ed033c1a24a9241e5d95ed45df201eade59605f
David Delassus <david.jose.delassus@gmail.com>
2021-05-23 22:53:52 +0200

    :sparkles: add skeleton for resource controller

--- apps/datapio_project_operator/lib/datapio_project_operator/controller.ex
+++ apps/datapio_project_operator/lib/datapio_project_operator/controller.ex
@@ -1,65 +1,132 @@
 defmodule DatapioProjectOperator.Controller do
-  @moduledoc """
-  Describe a Datapio Project
-  """
-  use Bonny.Controller
+  @moduledoc false
+
+  use GenServer
   alias DatapioProjectOperator.Resources, as: Resources
 
-  @group "datapio.co"
-  @version "v1"
+  @poll_delay 5000
+  @api_version "datapio.co/v1"
+  @kind :project
 
-  @scope :namespaced
-  @names %{
-    plural: "projects",
-    singular: "project",
-    kind: "Project",
-    shortNames: [
-      "proj"
-    ]
-  }
-
-  @doc """
-  Handles an `ADDED` event
-  """
-  @spec add(map()) :: :ok | :error
-  @impl Bonny.Controller
-  def add(%{} = project) do
-    desired_resources = Resources.from_project(project)
-    # TODO: kubectl apply
-    :ok
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, [args], name: __MODULE__)
   end
 
-  @doc """
-  Handles a `MODIFIED` event
-  """
-  @spec modify(map()) :: :ok | :error
-  @impl Bonny.Controller
-  def modify(%{} = project) do
-    reconcile(project)
+  @impl true
+  def init(_args) do
+    with {:ok, conn} <- K8s.Conn.lookup(:default)
+    do
+      send(self(), :list)
+      {:ok, [conn: conn, cache: %{}]}
+
+    else
+      err -> {:stop, err}
+    end
   end
 
-  @doc """
-  Handles a `DELETED` event.
+  @impl true
+  def handle_info(:list, state) do
+    conn = state[:conn]
+    cache = state[:cache]
+    pid = self()
 
-  Since we set the 'ownerReferences' metadata, Kubernetes will automatically
-  remove child resources.
-  """
-  @spec delete(map()) :: :ok | :error
-  @impl Bonny.Controller
-  def delete(%{} = project) do
-    :ok
+    K8s.Client.list(@api_version, @kind, namespace: :all)
+      # Execute Query
+      |> K8s.Client.run(conn)
+      # Parse API Server Response
+      |> (fn {:ok, %{ "items" => items }} -> items end).()
+      # Split items into added/modified
+      |> Stream.map(fn resource ->
+        %{ "metadata" => %{ "uid" => uid }} = resource
+
+        case cache[uid] do
+          nil ->
+            {:added, uid, resource}
+
+          _ ->
+            {:modified, uid, resource}
+        end
+      end)
+      # Build Map from added/modified items with removed from cache
+      |> Enum.reduce(
+        %{ added: %{}, modified: %{}, deleted: cache },
+        fn
+          {:added, uid, resource}, resources ->
+            %{
+              added: resources[:added] |> Map.put(uid, resource),
+              modified: resources[:modified],
+              deleted: resources[:deleted] |> Map.delete(uid)
+            }
+          {:modified, uid, resource}, resources ->
+            %{
+              added: resources[:added],
+              modified: resources[:modified] |> Map.put(uid, resource),
+              deleted: resources[:deleted] |> Map.delete(uid)
+            }
+        end
+      )
+      # Transform submaps into arrays
+      |> (fn resources -> %{
+        added: resources[:added] |> Map.values(),
+        modified: resources[:modified] |> Map.values(),
+        deleted: resources[:deleted] |> Map.values()
+      } end).()
+      # Send events
+      |> (fn resources ->
+        resources[:added] |> Enum.map(&send(self(), {:added, &1}))
+        resources[:modified] |> Enum.map(&send(self(), {:modified, &1}))
+        resources[:deleted] |> Enum.map(&send(self(), {:deleted, &1}))
+      end).()
+
+    Process.send_after(pid, :list, @poll_delay)
+
+    {:noreply, [conn: conn, cache: cache]}
   end
 
-  @doc """
-  Called periodically for each existing CustomResource to allow for reconciliation.
-  """
-  @spec reconcile(map()) :: :ok | :error
-  @impl Bonny.Controller
-  def reconcile(%{} = project) do
-    desired_resources = Resources.from_project(project)
-    # TODO: kubectl get by ownerReferences
-    # TODO: kubectl apply
-    # TODO: kubectl delete old
+  def handle_info({:added, resource}, state) do
+    IO.puts("ADDED #{resource["metadata"]["name"]}")
+    conn = state[:conn]
+    cache = state[:cache]
+
+    %{ "metadata" => %{ "uid" => uid }} = resource
+    :ok = resource |> reconcile(conn)
+
+    cache = cache |> Map.put(uid, resource)
+
+    {:noreply, [conn: conn, cache: cache]}
+  end
+
+  def handle_info({:modified, resource}, state) do
+
+    conn = state[:conn]
+    cache = state[:cache]
+
+    %{ "metadata" => %{ "uid" => uid, "resourceVersion" => new_ver }} = resource
+    %{ "metadata" => %{ "resourceVersion" => old_ver }} = cache[uid]
+
+    cache = if old_ver != new_ver do
+      IO.puts("MODIFIED #{resource["metadata"]["name"]}")
+      :ok = resource |> reconcile(conn)
+      cache |> Map.put(uid, resource)
+    else
+      cache
+    end
+
+    {:noreply, [conn: conn, cache: cache]}
+  end
+
+  def handle_info({:deleted, resource}, state) do
+    IO.puts("DELETED #{resource["metadata"]["name"]}")
+    conn = state[:conn]
+    cache = state[:cache]
+
+    %{ "metadata" => %{ "uid" => uid }} = resource
+    cache = cache |> Map.delete(uid)
+
+    {:noreply, [conn: conn, cache: cache]}
+  end
+
+  defp reconcile(resource, conn) do
     :ok
   end
 end
