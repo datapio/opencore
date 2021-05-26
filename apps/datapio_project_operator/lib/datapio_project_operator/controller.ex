7c98423ff694628ef8c614aa02c32289926f2909
David Delassus <david.jose.delassus@gmail.com>
2021-05-26 23:37:37 +0200

    :sparkles: add resource schema #project-operator

--- apps/datapio_project_operator/lib/datapio_project_operator/controller.ex
+++ apps/datapio_project_operator/lib/datapio_project_operator/controller.ex
@@ -3,7 +3,39 @@ defmodule DatapioProjectOperator.Controller do
 
   use Datapio.Controller,
     api_version: "datapio.co/v1",
-    kind: :project
+    kind: :project,
+    schema: schema(%{
+      "spec" => schema(%{
+        "ingress" => one_of([
+          schema(%{
+            "enabled" => false
+          }),
+          schema(%{
+            "enabled" => true,
+            "host" => spec(is_binary() and &(String.length(&1) > 0)),
+            "labels" => one_of([
+              nil,
+              spec(is_map())
+            ]),
+            "annotations" => one_of([
+              nil,
+              spec(is_map())
+            ]),
+            "tls" => one_of([
+              nil,
+              false,
+              spec(is_binary() and &(String.length(&1) > 0))
+            ])
+          })
+        ]),
+        "webhooks" => coll_of(schema(%{
+          "name" => spec(is_binary() and &(String.length(&1) > 0)),
+          "max_concurrent_jobs" => spec(is_integer() and &(&1 > 0)),
+          "history" => spec(is_integer() and &(&1 >= 0))
+        }))
+      })
+    })
+
 
   @impl true
   def add(resource) do
