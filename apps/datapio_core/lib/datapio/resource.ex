77e63ce45ba3a42a94059109f9938594eed5ab45
David Delassus <david.jose.delassus@gmail.com>
2021-05-27 17:32:39 +0200

    :bug: replace norm with jsonxema in resource module #core

--- apps/datapio_core/lib/datapio/resource.ex
+++ apps/datapio_core/lib/datapio/resource.ex
@@ -1,62 +1,115 @@
 defmodule Datapio.Resource do
-  use Norm
+  defp validate_owner_resource(%{} = owner) do
+    schema = %{
+      "type" => "object",
+      "required" => ["apiVersion", "kind", "metadata"],
+      "properties" => %{
+        "apiVersion" => %{ "type" => "string", "minLength" => 1},
+        "kind" => %{ "type" => "string", "minLength" => 1},
+        "metadata" => %{
+          "type" => "object",
+          "required" => ["name", "uid"],
+          "properties" => %{
+            "name" => %{ "type" => "string", "minLength" => 1},
+            "uid" => %{ "type" => "string", "minLength" => 1}
+          }
+        }
+      }
+    } |> JsonXema.new()
 
-  defp conform_owner_resource(%{} = owner) do
-    try do
-      {:ok, conform!(owner, schema(%{
-        "apiVersion" => spec(is_binary() and &(String.length(&1) > 0)),
-        "kind" => spec(is_binary() and &(String.length(&1) > 0)),
-        "metadata" => schema(%{
-          "name" => spec(is_binary() and &(String.length(&1) > 0)),
-          "uid" => spec(is_binary() and &(String.length(&1) > 0))
-        })
-      }))}
-    rescue
-      err -> {:error, {:owner, err}}
+    with :ok <- JsonXema.validate(schema, owner)
+    do
+      :ok
+    else
+      {:error, err} -> {:error, {:owner, err}}
     end
   end
 
-  defp conform_owned_resource(%{} = resource, %{} = owner) do
-    try do
-      {:ok, conform!(resource, schema(%{
-        "metadata" => schema(%{
-          "ownerReferences" => coll_of(schema(%{
-            "apiVersion" => spec(is_binary() and &(owner["apiVersion"] == &1)),
-            "kind" => spec(is_binary() and &(owner["kind"] == &1)),
-            "name" => spec(is_binary() and &(owner["metadata"]["name"] == &1)),
-            "uid" => spec(is_binary() and &(owner["metadata"]["uid"] == &1))
-          }))
-        })
-      }))}
-    rescue
-      err -> {:error, {:owned, err}}
+  defp validate_owned_resource(%{} = resource, %{}) do
+    schema = %{
+      "type" => "object",
+      "required" => ["metadata"],
+      "properties" => %{
+        "metadata" => %{
+          "type" => "object",
+          "properties" => %{
+            "ownerReferences" => %{
+              "type" => "array",
+              "default" => [],
+              "items" => %{
+                "type" => "object",
+                "required" => ["apiVersion", "kind", "name", "uid"],
+                "properties" => %{
+                  "apiVersion" => %{ "type" => "string", "minLength" => 1},
+                  "kind" => %{ "type" => "string", "minLength" => 1},
+                  "name" => %{ "type" => "string", "minLength" => 1},
+                  "uid" => %{ "type" => "string", "minLength" => 1}
+                }
+              }
+            }
+          }
+        }
+      }
+    } |> JsonXema.new()
+
+    with :ok <- JsonXema.validate(schema, resource)
+    do
+      :ok
+    else
+      {:error, err} -> {:error, {:owned, err}}
     end
   end
 
   def is_owned(%{} = resource, %{} = owner) do
-    with {:ok, _} <- conform_owner_resource(owner),
-         {:ok, _} <- conform_owned_resource(resource, owner)
+    with :ok <- validate_owner_resource(owner),
+         :ok <- validate_owned_resource(resource, owner)
     do
-      true
+      resource["metadata"]["ownerReferences"]
+        |> Stream.filter(fn ref ->
+          ref["apiVersion"] == owner["apiVersion"] and \
+          ref["kind"] == owner["kind"] and \
+          ref["name"] == owner["metadata"]["name"] and \
+          ref["uid"] == owner["metadata"]["uid"]
+        end)
+        |> Enum.count()
+        |> (fn i -> i > 0 end).()
     else
       {:error, {:owner, err}} ->
         raise err
 
-      _ ->
+      e ->
         false
     end
   end
 
   def list_contains(%{} = resource, items) do
-    resource_schema = schema(%{
-      "metadata" => schema(%{
-        "uid" => spec(is_binary() and &(String.length(&1) > 0))
-      })
-    })
+    resource_schema = %{
+      "type" => "object",
+      "required" => ["uid"],
+      "properties" => %{
+        "metadata" => %{
+          "type" => "object",
+          "required" => ["uid"],
+          "properties" => %{
+            "uid" => %{ "type" => "string", "minLength" => 1}
+          }
+        }
+      }
+    }
+    collection_schema = %{
+      "type" => "array",
+      "items" => resource_schema
+    }
 
-    resource = conform!(resource, resource_schema)
+    :ok = resource_schema
+       |> JsonXema.new()
+       |> JsonXema.validate!(resource)
 
-    conform!(items, coll_of(resource_schema))
+    :ok = collection_schema
+      |> JsonXema.new()
+      |> JsonXema.validate!(items)
+
+    items
       |> Stream.filter(fn item -> item["metadata"]["uid"] == resource["metadata"]["uid"] end)
       |> Enum.count()
       |> (fn i -> i > 0 end).()
