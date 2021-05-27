306189195f6eed47156127c0a08d6b7b9d3e1651
David Delassus <david.jose.delassus@gmail.com>
2021-05-27 20:46:31 +0200

    :white_check_mark: add test suite for Datapio.Resource.list_contains/2 #core

--- apps/datapio_core/test/datapio/resource_test.exs
+++ apps/datapio_core/test/datapio/resource_test.exs
@@ -1,7 +1,7 @@
 defmodule DatapioTest.Resource do
   use ExUnit.Case
 
-  describe "is_owned()" do
+  describe "is_owned/2" do
     test "should return true if there is an ownerReference" do
       resource = %{
         "metadata" => %{
@@ -61,6 +61,50 @@ defmodule DatapioTest.Resource do
 
       assert_raise JsonXema.ValidationError, fn ->
         Datapio.Resource.is_owned(resource, owner)
+      end
+    end
+  end
+
+  describe "list_contains/2" do
+    test "should return true if a resource with the same uid is found" do
+      resource = %{ "metadata" => %{ "uid" => "u1" } }
+      collection = [
+        %{ "metadata" => %{ "uid" => "u1" } },
+        %{ "metadata" => %{ "uid" => "u2" } }
+      ]
+
+      assert Datapio.Resource.list_contains(resource, collection) == true
+    end
+
+    test "should return true if no resource with the same uid is found" do
+      resource = %{ "metadata" => %{ "uid" => "u3" } }
+      collection = [
+        %{ "metadata" => %{ "uid" => "u1" } },
+        %{ "metadata" => %{ "uid" => "u2" } }
+      ]
+
+      assert Datapio.Resource.list_contains(resource, collection) == false
+    end
+
+    test "should raise an exception if the resources do not have a uid" do
+      resource = %{}
+      collection = [
+        %{ "metadata" => %{ "uid" => "u1" } },
+        %{ "metadata" => %{ "uid" => "u2" } }
+      ]
+
+      assert_raise JsonXema.ValidationError, fn ->
+        Datapio.Resource.list_contains(resource, collection)
+      end
+
+      resource = %{ "metadata" => %{ "uid" => "u1" } }
+      collection = [
+        %{},
+        %{ "metadata" => %{ "uid" => "u2" } }
+      ]
+
+      assert_raise JsonXema.ValidationError, fn ->
+        Datapio.Resource.list_contains(resource, collection)
       end
     end
   end
