d7c51d5fdf1a5b656882a5df9c5588059dcff2f5
David Delassus <david.jose.delassus@gmail.com>
2021-08-20 17:16:17 +0200

    :wrench: parametrize datapio configuration #core

--- apps/datapio_core/lib/datapio/cluster.ex
+++ apps/datapio_core/lib/datapio/cluster.ex
@@ -1,6 +1,24 @@
 defmodule Datapio.ClusterSupervisor do
   @moduledoc false
 
+  defp service_name do
+    var = Application.get_env(:datapio_core, :service_name, [
+      env: "DATAPIO_SERVICE_NAME",
+      default: nil
+    )
+    System.get_env(var[:env], var[:default])
+  end
+
+  defp app_name do
+    var = Application.get_env(:datapio_core, :app_name, [
+      env: "DATAPIO_APP_NAME",
+      default: "datapio-opencore"
+    ])
+    System.get_env(var[:env], var[:default])
+  end
+
+  defp cluster_opts, do: Application(:datapio_core, :cluster_opts, [])
+
   def child_spec(_args) do
     %{
       id: __MODULE__,
@@ -10,16 +28,16 @@ defmodule Datapio.ClusterSupervisor do
   end
 
   def start_link() do
-    topologies = case System.get_env("DATAPIO_SERVICE_NAME", nil) do
+    topologies = case service_name() do
       nil -> []
       svc -> [
         default: [
           strategy: Cluster.Strategy.Kubernetes.DNS,
           config: [
             service: svc,
-            application_name: System.get_env("DATAPIO_APP_NAME", "datapio-opencore")
+            application_name: app_name()
           ]
-        ]
+        ] |> Keyword.merge(cluster_opts())
       ]
     end
 
