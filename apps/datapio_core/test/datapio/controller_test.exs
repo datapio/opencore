27654231d29d8158f3b9658e9c32308c63190f71
David Delassus <david.jose.delassus@gmail.com>
2021-08-20 15:41:08 +0200

    :clown_face: add test controller skeleton #core

--- apps/datapio_core/test/datapio/controller_test.exs
+++ apps/datapio_core/test/datapio/controller_test.exs
@@ -1,3 +1,29 @@
+defmodule DatapioTest.Controller.TestController do
+  use Datapio.Controller,
+    api_version: "v1",
+    kind: :example
+
+  @impl true
+  def add(resource) do
+    :ok
+  end
+
+  @impl true
+  def modify(resource) do
+    :ok
+  end
+
+  @impl true
+  def delete(resource) do
+    :ok
+  end
+
+  @impl true
+  def reconcile(resource) do
+    :ok
+  end
+end
+
 defmodule DatapioTest.Controller do
   use ExUnit.Case
 end
