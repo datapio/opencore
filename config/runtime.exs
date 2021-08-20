9d7db8bf0842818ddc4c3c7f912a0fd7e26c6d97
David Delassus <david.jose.delassus@gmail.com>
2021-08-20 15:36:40 +0200

    :wrench: move runtime configuration to application code #core

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,28 +6,3 @@ config :datapio_project_operator,
 
 config :datapio_pipelinerun_server,
   rabbitmq_url: System.get_env("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/")
-
-
-case config_env() do
-  :prod ->
-    default_k8s_topology = case System.get_env("DATAPIO_SERVICE_NAME", nil) do
-      nil -> []
-      svc -> [
-        default: [
-          strategy: Cluster.Strategy.Kubernetes.DNS,
-          config: [
-            service: svc,
-            application_name: System.get_env("DATAPIO_APP_NAME", "datapio-opencore")
-          ]
-        ]
-      ]
-    end
-
-    config :datapio_core,
-      topology: default_k8s_topology
-
-  _ ->
-
-    config :datapio_core,
-      topology: []
-end
