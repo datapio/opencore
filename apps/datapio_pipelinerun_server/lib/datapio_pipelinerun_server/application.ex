8275e280a07122a8dfa23325a28c98bdea2a2dcc
David Delassus <david.jose.delassus@gmail.com>
2021-08-20 15:40:14 +0200

    :construction: add module skeleton #pipelinerun-server

--- apps/datapio_pipelinerun_server/lib/datapio_pipelinerun_server/application.ex
+++ apps/datapio_pipelinerun_server/lib/datapio_pipelinerun_server/application.ex
@@ -1,6 +1,4 @@
 defmodule DatapioPipelinerunServer.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,12 @@ defmodule DatapioPipelinerunServer.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: DatapioPipelinerunServer.Worker.start_link(arg)
-      # {DatapioPipelinerunServer.Worker, arg}
+      {Highlander, DatapioPipelinerunServer.Controller.Server},
+      {Highlander, DatapioPipelinerunServer.Controller.Request},
+      {Highlander, DatapioPipelinerunServer.Scheduler},
+      DatapioPipelinerunServer.WorkerPool
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: DatapioPipelinerunServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
