77ba104387b041a4bdfc6ddabe2e7ea97a8a9104
David Delassus <david.jose.delassus@gmail.com>
2021-08-20 17:16:17 +0200

    :wrench: parametrize datapio configuration #core

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,10 @@ config :logger, :console,
   format: "$date $time [$level] $metadata$message\n",
   metadata: [:user_id]
 
-config :datapio_core, mocks: []
+config :datapio_core,
+  service_name: [env: "DATAPIO_SERVICE_NAME", default: nil],
+  app_name: [env: "DATAPIO_APP_NAME", default: "datapio-opencore"],
+  cluster_opts: [],
+  mocks: []
 
 import_config "#{config_env()}.exs"
