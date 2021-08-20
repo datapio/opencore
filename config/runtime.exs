08448049ad9d009fe5db25ce27561035910695bb
David Delassus <david.jose.delassus@gmail.com>
2021-08-20 15:46:20 +0200

    :fire: remove runtime configuration

--- config/runtime.exs
+++
@@ -1,8 +0,0 @@
-import Config
-
-
-config :datapio_project_operator,
-  klifter_image: System.get_env("DATAPIO_KLIFTER_IMAGE", "ghcr.io/datapio/klifter:latest")
-
-config :datapio_pipelinerun_server,
-  rabbitmq_url: System.get_env("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/")
