c04ed497511432c4b56d4d672148b32243f20246
David Delassus <david.jose.delassus@gmail.com>
2021-08-23 23:35:14 +0200

    :sparkles: provide default value for KUBECONFIG env var #core

--- apps/datapio_core/lib/datapio/k8s_conn.ex
+++ apps/datapio_core/lib/datapio/k8s_conn.ex
@@ -5,8 +5,8 @@ defmodule Datapio.K8sConn do
 
   alias Datapio.Dependencies, as: Deps
 
-  def lookup() do
-    case System.get_env("KUBECONFIG") do
+  def lookup(default_path \\ nil) do
+    case System.get_env("KUBECONFIG", default_path) do
       nil -> Deps.get(:k8s_conn).from_service_account()
       path -> Deps.get(:k8s_conn).from_file(path)
     end
