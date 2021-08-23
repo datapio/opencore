7309ac14fbefeedfd5e21102ff4e9cf1cd61e312
David Delassus <david.jose.delassus@gmail.com>
2021-08-23 16:55:39 +0200

    :white_check_mark: :recycle: update Datapio.Dependencies test suite #core

--- apps/datapio_core/test/datapio/deps_test.exs
+++ apps/datapio_core/test/datapio/deps_test.exs
@@ -3,14 +3,15 @@ defmodule DatapioTest.Dependencies do
 
   alias Datapio.Dependencies, as: Deps
 
-  setup do
-    [ets_table: Deps.setup()]
-  end
-
   describe "dependency injection" do
     test "register/2" do
       Deps.register(Foo, Bar)
       assert Bar == Deps.get(Foo)
+    end
+
+    test "mocks" do
+      assert DatapioMock.K8s.Client == Deps.get(:k8s_client)
+      assert DatapioMock.K8s.Conn == Deps.get(:k8s_conn)
     end
   end
 end
