9230e67675cab1a5de55dd91ec8a48bbd2b8a332
David Delassus <david.jose.delassus@gmail.com>
2021-08-24 00:20:52 +0200

    :arrow_up: upgrade to elixir 1.12 and OTP 24

--- apps/datapio_core/mix.exs
+++ apps/datapio_core/mix.exs
@@ -9,7 +9,7 @@ defmodule DatapioLib.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
