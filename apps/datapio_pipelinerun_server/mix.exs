3e6c192e8ab39dafd735752deeb53f93cbef71db
David Delassus <david.jose.delassus@gmail.com>
2021-08-26 18:31:59 +0200

    :heavy_plus_sign: add amqp, highlander and horde #pipelinerun-server

--- apps/datapio_pipelinerun_server/mix.exs
+++ apps/datapio_pipelinerun_server/mix.exs
@@ -17,13 +17,17 @@ defmodule DatapioPipelinerunServer.MixProject do
 
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :datapio_core],
       mod: {DatapioPipelinerunServer.Application, []}
     ]
   end
 
   defp deps do
     [
+      {:datapio_core, in_umbrella: true},  # Datapio Core Library
+      {:highlander, "~> 0.2"},             # Ensure single process across cluster
+      {:horde, "~> 0.8"},                  # Load Balance workload across cluster
+      {:amqp, "~> 2.1"},                   # RabbitMQ client
     ]
   end
 end
