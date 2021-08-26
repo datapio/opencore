127a59ffaa4624826ce419013e472dc6576e8d60
David Delassus <david.jose.delassus@gmail.com>
2021-08-26 18:39:37 +0200

    :sparkles: :construction: add request module to manage PipelineRunRequest resources #pipelinerun-server

---
+++ apps/datapio_pipelinerun_server/lib/datapio_pipelinerun_server/request/utils.ex
@@ -0,0 +1,17 @@
+defmodule DatapioPipelineRunServer.Request.Utilities do
+  @moduledoc """
+  Utility functions for the PipelineRunRequest controller.
+  """
+
+  def schedule_request(request) do
+  end
+
+  def archive_request(request) do
+  end
+
+  def cancel_request(request) do
+  end
+
+  def reschedule_request(request) do
+  end
+end
