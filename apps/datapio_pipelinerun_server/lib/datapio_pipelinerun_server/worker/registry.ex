706acb91c40d12938a7cfc0428598517770c52f3
David Delassus <david.jose.delassus@gmail.com>
2021-08-26 18:38:19 +0200

    :sparkles: :construction: add worker module to handle pipelineruns execution #pipelinerun-server

---
+++ apps/datapio_pipelinerun_server/lib/datapio_pipelinerun_server/worker/registry.ex
@@ -0,0 +1,19 @@
+defmodule DatapioPipelineRunServer.Worker.Registry do
+  @moduledoc """
+  Global Elixir process registry for the worker pool
+  """
+
+  use Horde.Registry
+
+  def start_link(_args) do
+    Horde.Registry.start_link(
+      __MODULE__,
+      [keys: :unique, members: :auto],
+      name: __MODULE__
+    )
+  end
+
+  def init(args) do
+    Horde.Registry.init(args)
+  end
+end
