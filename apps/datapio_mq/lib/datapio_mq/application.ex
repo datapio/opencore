77dde3fb38919f3850d2a589dfdfc655f906ed3d
David Delassus <david.jose.delassus@gmail.com>
2021-08-27 17:27:03 +0200

    :sparkles: add members param to horde #mq

--- apps/datapio_mq/lib/datapio_mq/application.ex
+++ apps/datapio_mq/lib/datapio_mq/application.ex
@@ -6,8 +6,8 @@ defmodule Datapio.MQ.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      {Horde.Registry, [name: Datapio.MQ.Registry, keys: :unique]},
-      {Horde.DynamicSupervisor, [name: Datapio.MQ.Pool, strategy: :one_for_one]}
+      {Horde.Registry, [name: Datapio.MQ.Registry, keys: :unique, members: :auto]},
+      {Horde.DynamicSupervisor, [name: Datapio.MQ.Pool, strategy: :one_for_one, members: :auto]}
     ]
 
     opts = [strategy: :one_for_one, name: Datapio.MQ.Supervisor]
