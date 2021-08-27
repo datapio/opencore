f8b1bf79d8ebb403eabcbb60f51b4695e0675f71
David Delassus <david.jose.delassus@gmail.com>
2021-08-27 17:27:45 +0200

    :sparkles: :bug: add data to consumer behavior and fix call to queue functions #mq

--- apps/datapio_mq/lib/datapio_mq/consumer.ex
+++ apps/datapio_mq/lib/datapio_mq/consumer.ex
@@ -1,48 +1,54 @@
 defmodule Datapio.MQ.Consumer do
-  @callback handle_message(term()) :: :ack | :nack
-  @callback handle_shutdown() :: :ok
+  @callback handle_message(term(), term()) :: :ack | :nack
+  @callback handle_shutdown(term()) :: :ok
 
-  defmacro __using__(opts) do
-    queue_name = opts |> Keyword.fetch!(:queue)
+  defstruct [:queue, :data]
 
+  defmacro __using__(_opts) do
     quote do
       use GenServer, restart: :transient
 
-      @behavior Datapio.MQ.Consumer
+      @behaviour Datapio.MQ.Consumer
 
-      def start_link(args) do
-        GenServer.start_link(__MODULE__, args, name: __MODULE__)
+      def start_link(options) do
+        GenServer.start_link(__MODULE__, options, name: __MODULE__)
       end
 
       @impl true
-      def init(_args) do
+      def init(options) do
+        state = %Datapio.MQ.Consumer{
+          queue: options |> Keyword.fetch!(:queue),
+          data: options |> Keyword.get(:data, nil)
+        }
+
         send(self(), :drain)
-        {:ok, unquote(queue_name)}
+        {:ok, state}
       end
 
       @impl true
-      def handle_info(:drain, queue_name) do
-        :ok = Datapio.MQ.drain(queue_name)
-        {:noreply, queue_name}
+      def handle_info(:drain, %Datapio.MQ.Consumer{} = state) do
+        :ok = Datapio.MQ.Queue.drain(state.queue)
+        {:noreply, state}
       end
 
       @impl true
-      def handle_info({:datapio_mq_consume, message}, queue_name) do
-        case apply(__MODULE__, :handle_message, [message]) do
+      def handle_info({:datapio_mq_consume, message}, %Datapio.MQ.Consumer{} = state) do
+        :ok = case apply(__MODULE__, :handle_message, [message, state.data]) do
           :ack ->
             :ok
 
           :nack ->
-            Datapio.MQ.publish(queue_name, message)
+            Datapio.MQ.Queue.publish(state.queue, message)
         end
+
         send(self(), :drain)
-        {:noreply, queue_name}
+        {:noreply, state}
       end
 
       @impl true
-      def handle_info(:datapio_mq_shutdown, queue_name) do
-        :ok = apply(__MODULE__, :handle_shutdown, [])
-        {:stop, :normal, queue_name}
+      def handle_info(:datapio_mq_shutdown, %Datapio.MQ.Consumer{} = state) do
+        :ok = apply(__MODULE__, :handle_shutdown, [state.data])
+        {:stop, :normal, state}
       end
     end
   end
