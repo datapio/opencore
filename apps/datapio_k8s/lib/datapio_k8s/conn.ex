f29757e72bc569af6a568fd4cc142f2e1e83a3b8
David Delassus <david.jose.delassus@gmail.com>
2021-09-11 23:13:34 +0200

    :label: fix Datapio.K8s.Conn.lookup/1 typespec #k8s

--- apps/datapio_k8s/lib/datapio_k8s/conn.ex
+++ apps/datapio_k8s/lib/datapio_k8s/conn.ex
@@ -4,7 +4,7 @@ defmodule Datapio.K8s.Conn do
   """
 
   @doc "Generate a `K8s.Conn` object from a *kubeconfig* or in-cluster config."
-  @spec lookup(String.t() | nil) :: K8s.Conn.t()
+  @spec lookup(String.t() | nil) :: {:ok, K8s.Conn.t()} | {:error, term()}
   def lookup(default_path \\ nil) do
     case System.get_env("KUBECONFIG", default_path) do
       nil -> K8s.Conn.from_service_account()
