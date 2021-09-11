a8946880b7a9f35bd994cea37dd9646ddbacdc6c
David Delassus <david.jose.delassus@gmail.com>
2021-09-11 23:14:08 +0200

    :label: :memo: Add docstrings and typespecs #mq

--- apps/datapio_mq/lib/datapio_mq.ex
+++ apps/datapio_mq/lib/datapio_mq.ex
@@ -1,13 +1,20 @@
 defmodule Datapio.MQ do
   @moduledoc """
-  Documentation for `DatapioMq`.
+  Message Queue application interface.
   """
 
+  @type queue_name :: Datapio.MQ.Queue.queue_name()
+  @type consumer_options  :: Datapio.MQ.Consumer.consumer_options()
+
+  @doc "Start a new queue"
+  @spec start_queue(queue_name()) :: DynamicSupervisor.on_start_child()
   def start_queue(queue_name) do
-    child = {Datapio.MQ.Queue, [queue_name]}
+    child = {Datapio.MQ.Queue, queue_name}
     Horde.DynamicSupervisor.start_child(Datapio.MQ.Pool, child)
   end
 
+  @doc "Start a consumer"
+  @spec start_consumer(consumer_options()) :: DynamicSupervisor.on_start_child()
   def start_consumer(options) do
     module = options |> Keyword.fetch!(:module)
     child = {module, options}
